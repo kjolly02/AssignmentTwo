5892224893092aa2d3a9c6c9e977082e93429cf0
Santiago Gutierrez <ssgutierrez42@gmail.com>
2020-12-26 11:34:58 -0800

    Initial Commit

---
+++ Assignment Two/Assignment_TwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_TwoApp.swift
+//  Assignment Two
+//
+//  Created by Santiago Gutierrez on 12/26/20.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_TwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
